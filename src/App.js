3025e49187f90cc1e82d450f3349f1ffc9772d2c
Thiago Bastos <thiagobuni7@gmail.com>
2020-11-15 19:59:43 -0300

    Main page code moved into own component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,13 @@
 import './App.css';
-import './components/recipe/Recipe'
+import Main from './components/main/Main'
 import React, { useState } from 'react'
-import Recipe from './components/recipe/Recipe';
+
 
 function App() {
   
-  const [searchValue, setSearchValue] = useState('');
-  const [recipes, setRecipes] = useState(undefined);
-
-  const API_KEY = "your edamam api key";
-  const API_ID = "your edamam api id";
-  
-  const onSubmitButton = async e =>
-  {
-    e.preventDefault();
-    //Make a request to edamam API. 
-    const result = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=${API_ID}&app_key=${API_KEY}`);
-    //Turns the result into a JSON.
-    const data = await result.json();
-    setRecipes(data.hits);
-  }
-
   return (
-    <div className="App">
-      <form className="search-form">
-        <input 
-          className="search-bar"
-          placeholder="Search food" 
-          onChange={e => setSearchValue(e.target.value)}
-        ></input>
-        <button className="search-button" onClick={onSubmitButton}>Search</button>
-      </form>
-
-
-      <div className="recipes">
-        { typeof recipes === "undefined" ? <h1></h1>
-         : recipes.map(item =>
-          (
-            <Recipe 
-            key={item.recipe.label}
-            title={item.recipe.label}
-            image={item.recipe.image}
-            ingredients={item.recipe.ingredients}
-            />
-          ))}
-      </div>
-
-      {typeof recipes !== "undefined" ? (() => {
-        //I'm using a self called function to avoid double ternary expression.
-        if (recipes.length === 0) {
-          return <h1 className="recipes-found">No recipes found</h1>;
-        }
-      })()
-      : <h1 className="have-searched">You haven't searched recipes yet</h1>}
-
+    <div className="App"> 
+      <Main />
     </div>
   );
 }
---
+++ src/components/main/Main.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Recipe from '../recipe/Recipe';
+import dotenv from 'dotenv'
+dotenv.config();
+
+
+export default function Main()
+{
+  const [searchValue, setSearchValue] = useState('');
+  const [recipes, setRecipes] = useState(undefined);
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_ID = process.env.REACT_APP_API_ID;
+  
+  const onSubmitButton = async e =>
+  {
+    e.preventDefault();
+    //Make a request to edamam API. 
+    const result = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=${API_ID}&app_key=${API_KEY}`);
+    //Turns the result into a JSON.
+    const data = await result.json();
+    setRecipes(data.hits);
+  }
+
+  return (
+    <div className="Main">
+      <form className="search-form">
+        <input 
+          className="search-bar"
+          placeholder="Search food" 
+          onChange={e => setSearchValue(e.target.value)}
+        ></input>
+        <button className="search-button" onClick={onSubmitButton}>Search</button>
+      </form>
+
+
+      <div className="recipes">
+        { typeof recipes === "undefined" ? <h1></h1>
+         : recipes.map(item =>
+          (
+            <Recipe 
+            key={item.recipe.label}
+            title={item.recipe.label}
+            image={item.recipe.image}
+            ingredients={item.recipe.ingredients}
+            />
+          ))}
+      </div>
+
+      {typeof recipes !== "undefined" ? (() => {
+        //I'm using a self called function to avoid double ternary expression.
+        if (recipes.length === 0) {
+          return <h1 className="recipes-found">No recipes found</h1>;
+        }
+      })()
+      : <h1 className="have-searched">You haven't searched recipes yet</h1>}
+
+    </div>
+    );
+}
